87279bf61cde6e6fda8349bad5216d8c19619085
alejandrobarranco <alejandro.barranco@geoenlace.com>
2019-08-01 12:41:10 -0500

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Alejandro Barranco on 8/1/19.
+//  Copyright © 2019 Alejandro Barranco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
